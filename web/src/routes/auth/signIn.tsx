0f03843f20cffac8f2afe41d95938a2e2f444b61
alem43 <begunicalem6@gmail.com>
2026-03-08 22:41:37 +0100

    Sign in page frontend made for desktop only

--- web/src/routes/auth/signIn.tsx
+++ web/src/routes/auth/signIn.tsx
@@ -1,17 +1,144 @@
 import { createFileRoute } from '@tanstack/react-router'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { Link } from '@tanstack/react-router'
 import Header from '@/components/Header'
+import backgroundImage from '../../images/background-image.png'
+import backgroundImageBig from '../../images/background-image-bigscreen.png'
+import profilePictureDefault from '../../images/profile-picture-default.png'
+import mobileProfilePictureDefault from '../../images/mobile-profile-picture-default.svg'
+import geotaggerLogo from '../../images/geotagger-logo.svg'
+import logoBig from '../../images/logo-big.png'
+import geotaggerPatter from '../../images/geotagger-patern.png'
 
 export const Route = createFileRoute('/auth/signIn')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
+  const signUpValuesSchema = z
+    .object({
+      email: z.string().email(),
+      firstName: z
+        .string()
+        .min(3, 'First name must be at least 3 characters')
+        .max(255),
+      lastName: z
+        .string()
+        .min(3, 'Last name must be at least 3 characters')
+        .max(255),
+      password: z
+        .string()
+        .min(6, 'Password must be at least 6 characters')
+        .max(20),
+      confirmPassword: z.string(),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+      message: 'Passwords do not match',
+      path: ['confirmPassword'],
+    })
+
+  type SignUpValues = z.infer<typeof signUpValuesSchema>
+
+  const ErrorText = ({ children }: { children?: string }) => (
+    <>{children && <p className="text-xs text-red-500 pt-1">{children}</p>}</>
+  )
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignUpValues>({
+    resolver: zodResolver(signUpValuesSchema),
+  })
+
   return (
     <>
       <div className="xl:hidden">
         <Header />
       </div>
-      Hello "/auth/signIn"!
+
+      <div
+        style={{ backgroundImage: `url(${backgroundImage})` }}
+        className="bg-cover bg-center xl:p-0 px-8.75 pt-14 pb-24.75 max-w-360 mx-auto flex "
+      >
+        <div className="flex w-full bg-white max-w-86 sm:max-w-118.75 md:max-w-137.5 lg:max-w-156.25 xl:max-w-155 xl:h-screen xl:rounded-none xl:mx-0 rounded-4xl pt-11.5 pl-17.5 gap-28 flex-col box-shadow">
+          <Link to="/">
+            <img
+              src={geotaggerLogo}
+              alt="Geotagger logo"
+              className="cursor-pointer w-full max-w-42.75"
+            />
+          </Link>
+          <div className="flex flex-col items-center w-max pl-10.5">
+            <div className="flex flex-col gap-2 xl:gap-4 items-center-safe  ">
+              <h2 className="header-h4 text-[2.188rem] text-dark xl:text-[3.0625rem]">
+                Sign in
+              </h2>
+              <p
+                className="body-p text-center xl:max-w-101.25"
+                style={{ color: `#322D38` }}
+              >
+                Welcome back to Geotagger. We are glad that you are back.
+              </p>
+            </div>
+            <form
+              onSubmit={handleSubmit((data) => {
+                console.log(data)
+              })}
+              className="flex flex-col gap-4 w-full max-w-105"
+            >
+              <div className="flex flex-col gap-2 w-full">
+                <label htmlFor="email" className="input-label-form">
+                  Email
+                </label>
+                <input
+                  {...register('email')}
+                  type="text"
+                  className="input-field-form"
+                  id="email"
+                />
+                <ErrorText>{errors.email?.message}</ErrorText>
+              </div>
+              <div className="flex flex-col gap-2 w-full">
+                <label htmlFor="password" className="input-label-form">
+                  Password
+                </label>
+                <input
+                  {...register('password')}
+                  type="password"
+                  className="input-field-form"
+                  id="password"
+                />
+                <ErrorText>{errors.password?.message}</ErrorText>
+              </div>
+              <button type="submit" className="sign-in-primary">
+                Sign up
+              </button>
+              <div className="flex justify-between items-center">
+                <p className="body-p text-dark">
+                  Do you want to create an account?
+                </p>
+                <Link to="/auth/signIn" className="text-primary cursor-pointer">
+                  Sign up
+                </Link>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div
+          className="relative w-full h-screen flex justify-center-safe items-center-safe bg-cover bg-center"
+          style={{ backgroundImage: `url(${backgroundImageBig})` }}
+        >
+          <img src={logoBig} alt="big logo" />
+          <img
+            src={geotaggerPatter}
+            alt="geotagger pattern"
+            className="absolute top-0 left-0 w-full h-full"
+          />
+        </div>
+      </div>
     </>
   )
 }
